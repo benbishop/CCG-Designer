3aaeaa55ca0b0fe2ec491fa1c9dd49be0de911b6
Ben Bishop <ben@rendr.io>
2019-03-31 07:10:52 -0400

    Filled in Roll logic for Die.

---
+++ CCG.Core/Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CCG.Core
+{
+    public class Die
+    {
+        private static Random Ran = new Random();
+        
+        protected readonly uint Sides;
+        
+        public Die(uint sides = 6){
+            Sides = sides;
+        }   
+        
+        public uint Roll(){
+            return (uint)Ran.Next(1, (int)Sides + 1);
+        }
+    }
+}
