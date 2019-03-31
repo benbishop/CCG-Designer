6b486fbc3c8da6fd6e91d0d9a1cd1fc5346fb029
Ben Bishop <ben@rendr.io>
2019-03-30 20:26:07 -0400

    Added units field to Card Attribute.

--- CCG.Core/CardAttribute.cs
+++ CCG.Core/CardAttribute.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-
-namespace CCG.Core
-{
-    public struct CardAttribute
-    {
-        public string Name;
-        public int Value;
-        
-        public CardAttribute(string name, int value){
-            Name = name;
-            Value = value;
-        }
-    }
-}
+CCG/CCG.Core/CardAttribute.cs
