9e906389c45bceaf6bdd59938c78ae70f716e1d8
Ben Bishop <ben@rendr.io>
2019-03-31 10:47:31 -0400

    Created Counter class to act as a tracker of

---
+++ CCG.Core/Counter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CCG.Core
+{
+    public class Counter
+    {
+        protected uint Count = 0;
+        
+        public uint Value{ get => Count; }
+        
+        public Counter(uint initialValue = 0){
+            Count = initialValue;
+        }
+        
+        public void Increment(uint amount = 1) => Count += amount;
+        
+        public void Decrement(uint amount = 1){
+            if(Count - amount < 0)
+                throw new CounterException();
+                
+            Count -= amount;
+        }
+    }
+    
+    public class CounterException:Exception{
+        public CounterException():base("Counter cannot have a value less than 0"){
+            
+        }
+    }
+}
