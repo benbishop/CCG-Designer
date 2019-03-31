eb2b8e273b707d8cee0d2e188ade60e7f0267581
Ben Bishop <ben@rendr.io>
2019-03-31 11:45:27 -0400

    Added play validations and actions.
    Added arrays of validations and actions to a card to be invoked when a
    player attempts to play a card.

--- CCG.Core/Card.cs
+++ CCG.Core/Card.cs
@@ -7,9 +7,8 @@ namespace CCG.Core
 {
     public struct Card
     {
-        public const string NoDescription = "";
         
-
+        public const string NoDescription = "";
         
         public readonly string Title;
         
@@ -17,17 +16,26 @@ namespace CCG.Core
         
         public readonly CardAttribute[] Attributes;
         
+        public readonly Action[] PlayValidations;
+        
+        public readonly Action[] PlayActions;
+        
         public Card(string title,
                     CardAttribute[] attributes,
+                    Action[] playValidations,
+                    Action[] playActions,
                     string description = NoDescription){
             Title = title;
             Description = description;
             Attributes = attributes ?? new CardAttribute[0];
-           
+            PlayValidations = playValidations ?? new Action[0];
+            PlayActions = playActions ?? new Action[0];
             
             if(title == null)
                 throw new ArgumentNullException("Title cannot be null");                                
                 
         }
     }
+    
+    
 }
--- CCG.Core/CardBuilder.cs
+++ CCG.Core/CardBuilder.cs
@@ -9,11 +9,17 @@ namespace CCG.Core
     {
         protected List<CardAttribute> Attributes = new List<CardAttribute>();
         
+        protected List<Action> PlayValidations = new List<Action>();
+        
+        protected List<Action> PlayActions = new List<Action>();
+        
         protected string Title = "";
         
         protected string Description = "";
         
-        public CardBuilder WithAttribute(string name, int value, string units){
+        public CardBuilder WithAttribute(string name, 
+                                         int value = CardAttribute.NoValue, 
+                                         string units = null){
             
             Attributes.Add(new CardAttribute(name, value, units));
             
@@ -33,7 +39,11 @@ namespace CCG.Core
         }
         
         public Card Build(){
-            return new Card(Title, Attributes.ToArray(), Description);
+            return new Card(Title, 
+            Attributes.ToArray(), 
+            PlayValidations.ToArray(),
+            PlayActions.ToArray(),
+            Description);
         }
     }
 }
