7cf9cfd6363c225df9eacf77dd55e966d5abadb8
Ben Bishop <ben@rendr.io>
2019-03-31 12:42:58 -0400

    Started Sportsball.Basketball project
    Created project to start validating design.

---
+++ Sportsball.Basketball/BasketballPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Sportsball.Basketball
+{
+    public static class BasketballPlayer
+    {
+        public const string ReboundMinRoll = "Basketball_Player_Rebound_Min_Roll";
+    }
+}
---
+++ Sportsball.Basketball/PlayerCardBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CCG.Core;
+
+namespace Sportsball.Basketball
+{
+    public class PlayerCardBuilder:CardBuilder
+    {
+        public PlayerCardBuilder WithRebound(uint minRoll){
+            
+            WithAttribute(BasketballPlayer.ReboundMinRoll, (int)minRoll);
+            
+            return this;
+        }        
+        
+        public PlayerCardBuilder WithStamina(uint stamina){
+            //todo: add this
+            return this;
+        }
+    }
+}
