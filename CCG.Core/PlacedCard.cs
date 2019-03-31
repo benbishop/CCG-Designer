0715620cb2330d30456ca7638c43b5b45abecd15
Ben Bishop <ben@rendr.io>
2019-03-31 12:49:26 -0400

    Started PlacedCard class.

---
+++ CCG.Core/PlacedCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Library
+{
+    public class PlacedCard
+    {
+        private bool faceUp = false;
+	    public bool IsFaceUp{ get => faceUp; }
+	    
+	    public void TurnFaceUp() => faceUp = true;
+	    
+	    public void TurnFaceDown() => faceUp = false;
+	    
+	    
+    }
+}
