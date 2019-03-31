bac6b09bea385442ff05badf86bfb8a1aa4a58d4
Ben Bishop <ben@rendr.io>
2019-03-31 07:23:31 -0400

    Created Hand class.

---
+++ CCG.Core/Hand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CCG.Core
+{
+    public class Hand
+    {
+        private static Random Ran = new Random();
+        
+        protected readonly List<Card> Cards = new List<Card>();
+        
+        public void Add(Card card){
+            Cards.Add(card);
+        }
+        
+        public void Discard(Card card){
+            Cards.Remove(card);
+        }
+        
+        public Card PickRandom(){
+            return Cards[Ran.Next(0, Cards.Count)];
+        }
+    }
+}
