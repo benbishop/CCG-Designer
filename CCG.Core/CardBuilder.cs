2fd1b1a29dc1f6a804b16d84941e68959baba17c
Ben Bishop <ben@rendr.io>
2019-03-31 11:11:03 -0400

    Created CardBuilder class.

---
+++ CCG.Core/CardBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CCG.Core
+{
+    public class CardBuilder
+    {
+        protected List<CardAttribute> Attributes = new List<CardAttribute>();
+        
+        protected string Title = "";
+        
+        protected string Description = "";
+        
+        public CardBuilder WithAttribute(string name, int value, string units){
+            
+            Attributes.Add(new CardAttribute(name, value, units));
+            
+            return this;
+        }
+        
+        public CardBuilder WithTitle(string title)
+        {
+            Title = title;
+            return this;
+        }
+        
+        public CardBuilder WithDescription(string description)
+        {
+            Description = description;
+            return this;
+        }
+        
+        public Card Build(){
+            return new Card(Title, Attributes.ToArray(), Description);
+        }
+    }
+}
